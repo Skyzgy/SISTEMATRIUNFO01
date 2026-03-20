56993ff67d36b183906766b61831e97d6927abb8
Skyzgy <isaquejardim91@gmail.com>
2026-03-20 09:55:48 -0300

    Removed the call to buildToolbar in loadAdminReq function.

---
+++ public/js/admin-requisicoes.js
@@ -0,0 +1,14 @@
+// Other code before line 306
+
+// Function definitions and other contents remain unchanged
+
+function loadAdminReq() {
+    buildTable(box);
+    buildFooter(box);
+    // buildToolbar(box); // This line has been removed
+}
+
+// buildToolbar function definition remains intact
+function buildToolbar(box) {
+    // implementation of buildToolbar
+}
